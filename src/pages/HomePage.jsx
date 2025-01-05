782eb769230497c539a78557c9633222c456ce5b
nadolf <96559304+nadolf@users.noreply.github.com>
2025-01-05 01:44:24 -0500

    Update Project Image & Update sizing and spacing

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,7 +4,7 @@ import { FiLinkedin, FiGithub } from "react-icons/fi";
 export default function Home() {
   return (
     <div className="container">
-      <div className="content">
+      <div id="home" className="content">
         <h2 className="header">Hi! I'm</h2>
         <h1 className="header-name">Nadolf Tagoe</h1>
         <h3 className="job-title">Software Engineer</h3>
