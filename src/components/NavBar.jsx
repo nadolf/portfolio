6c2607d6db01024df920d96db5f39073ff23eff7
nadolf <96559304+nadolf@users.noreply.github.com>
2025-01-22 17:21:55 -0500

    Change Logo

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,13 @@
 import "../styles/NavBar.css";
 import resume from "../assets/Tagoe_Nadolf_Resume.pdf";
+import logo from "../assets/tab-logo.png";
 import { Link } from "react-scroll";
 import { useMode } from "../components/ModeContext";
-import { MdOutlineLightMode, MdOutlineModeNight, MdDehaze } from "react-icons/md";
+import {
+  MdOutlineLightMode,
+  MdOutlineModeNight,
+  MdDehaze,
+} from "react-icons/md";
 import { useState } from "react";
 
 export default function NavigationBar() {
@@ -15,28 +20,55 @@ export default function NavigationBar() {
 
   return (
     <div className={`navbar-container ${mode}`}>
-      <h2 className="navigation-header">Nadolf</h2>
-      
+      <div className="navigation-logo">
+        <img className="logo-image" src={logo} alt="headshot" />
+      </div>
       {/* Hamburger icon */}
       <div className={`hamburger-icon ${mode}`} onClick={toggleMenu}>
         <MdDehaze size={30} />
       </div>
 
-      <div className={`navigation-container ${isMenuOpen ? "active" : ""} ${mode}`}>
-        <Link to="home" smooth={true} duration={800} className={`nav-link ${mode}`}>
+      <div
+        className={`navigation-container ${isMenuOpen ? "active" : ""} ${mode}`}
+      >
+        <Link
+          to="home"
+          smooth={true}
+          duration={800}
+          className={`nav-link ${mode}`}
+        >
           Home
         </Link>
-        <Link to="about" smooth={true} duration={800} className={`nav-link ${mode}`}>
+        <Link
+          to="about"
+          smooth={true}
+          duration={800}
+          className={`nav-link ${mode}`}
+        >
           About
         </Link>
-        <Link to="project" smooth={true} duration={800} className={`nav-link ${mode}`}>
+        <Link
+          to="project"
+          smooth={true}
+          duration={800}
+          className={`nav-link ${mode}`}
+        >
           Projects
         </Link>
-        <a href={resume} target="_blank" rel="noopener noreferrer" className={`nav-link ${mode}`}>
+        <a
+          href={resume}
+          target="_blank"
+          rel="noopener noreferrer"
+          className={`nav-link ${mode}`}
+        >
           Resume
         </a>
         <div className="nav-icons" onClick={toggleMode}>
-          {mode === "light" ? <MdOutlineLightMode size={25} /> : <MdOutlineModeNight size={25} />} 
+          {mode === "light" ? (
+            <MdOutlineLightMode size={25} />
+          ) : (
+            <MdOutlineModeNight size={25} />
+          )}
         </div>
       </div>
     </div>
